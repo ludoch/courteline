d322d188818a917d05c6d3cb6956a73943a8c09d
Ludovic Champenois <ludo@google.com>
2022-06-19 10:31:07 +0200

    Update site.

--- src/main/java/com/google/appengine/demos/courteline/VerifyRecaptcha.java
+++ src/main/java/com/google/appengine/demos/courteline/VerifyRecaptcha.java
@@ -17,8 +17,7 @@ import javax.net.ssl.HttpsURLConnection;
 public class VerifyRecaptcha {
 
     private static final String url = "https://www.google.com/recaptcha/api/siteverify";
-    private static final String client = "6LeqZg0gAAAAAGqBnbZxJPuQmS6R1a4DJ7LWXdcV";
-    private static final String secret = "6LeqZg0gAAAAAIJU-iQheU14aIA1SmHhft1rQlFJ";
+    private static final String client = "6LfWLIEgAAAAADoCmpeyrxRq_81uPpyhGGqQxXTX";
     private final static String USER_AGENT = "Mozilla/5.0";
 
     public static boolean verify(String gRecaptchaResponse) throws IOException {
